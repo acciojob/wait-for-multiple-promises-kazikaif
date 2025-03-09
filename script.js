317e38d563064d2a2f3c5bfab9443bdcaa4a3aab
Kazi Mohammad Kaif <130026107+kazikaif@users.noreply.github.com>
2025-03-09 12:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+function getRandomTime() {
+  return Math.random() * 2 + 1;
+}
+
+const promises = [
+  new Promise((resolve) => {
+    const time = getRandomTime();
+    setTimeout(() => resolve({ name: "Promise 1", time: time.toFixed(3) }), time * 1000);
+  }),
+  new Promise((resolve) => {
+    const time = getRandomTime();
+    setTimeout(() => resolve({ name: "Promise 2", time: time.toFixed(3) }), time * 1000);
+  }),
+  new Promise((resolve) => {
+    const time = getRandomTime();
+    setTimeout(() => resolve({ name: "Promise 3", time: time.toFixed(3) }), time * 1000);
+  })
+];
+
+Promise.all(promises).then((results) => {
+  const output = document.getElementById("output");
+  output.innerHTML = "";
+
+  results.forEach(({ name, time }) => {
+    const row = `<tr><td>${name}</td><td>${time}</td></tr>`;
+    output.innerHTML += row;
+  });
+
+  const totalTime = Math.max(...results.map((r) => parseFloat(r.time))).toFixed(3);
+  output.innerHTML += `<tr class="table-info"><td>Total</td><td>${totalTime}</td></tr>`;
+});
